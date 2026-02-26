de40551477cb345792b559b830ef393e2acc4cec
yanaaa-it <yaninalikemiki@gmail.com>
2026-02-26 20:03:25 +0300

    Update logic.cpp

--- task02/logic.cpp
+++ task02/logic.cpp
@@ -1,12 +1,10 @@
 #include"logic.h"
 bool find_mirror(int array[DEFAULT_SIZE], int length) {
-	int next =length / 2;
-	for (int last = length / 2; last >= 0; last--)
+	for (int last = 0; last >= 0; last--)
 	{
-		if (array[last] != array[next] || length %2== 0) {
+		if (array[last] != array[last+1]) {
 			return false;
 		}
-		next++;
 	}
 	return true;
 }
